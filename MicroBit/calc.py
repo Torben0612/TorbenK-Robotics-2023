93f988df65d3bab93f024f2f8c5e3ea8b03abf2b
s3hMC <54605711+s3hMC@users.noreply.github.com>
2023-04-28 11:13:30 -0400

    Update calc.py

--- MicroBit/calc.py
+++ MicroBit/calc.py
@@ -1,7 +1,9 @@
 from microbit import *
-import time
 
 second_number = False
+first = 0
+second = 0
+
 
 numbers = [0,1,2,3,4,5,6,7,8,9]
 numbers_index = 0
@@ -29,31 +31,55 @@ DIVIDE = ('00900:'
 operators = [PLUS, MINUS, DIVIDE, TIMES]
 operators_index = 0
 
-while True:
-    if accelerometer.was_gesture('shake'):
-        second_number = True
-    if button_a.is_pressed() and button_b.is_pressed():
-        while button_a.is_pressed() or button_b.is_pressed():
-            pass
-        operators_index += 1
-        display.show(Image(operators[operators_index % len(operators)]))
-    elif button_a.was_pressed():
-        time.sleep(.2)
-        if not button_b.was_pressed():
-            numbers_index -= 1
-            display.show(numbers[numbers_index % len(numbers)])
-    elif button_b.was_pressed():
-        time.sleep(.2)
-        if not button_a.was_pressed():
-            numbers_index += 1
-            display.show(numbers[numbers_index % len(numbers)])
-
-def operate(first, second, operator):
+def operate():
     if operators_index % len(operators) == 0:
         return first+second
-    elif operators_index % len(operator) == 1:
+    elif operators_index % len(operators) == 1:
         return first-second
-    elif operator_index % len(operator) == 2:
+    elif operators_index % len(operators) == 2:
         return first*second
     else:
         return first/second
+
+display.show(0)
+while True:
+    if accelerometer.was_gesture('shake'):
+        if not second_number:
+            second_number = True
+            numbers_index = 0
+            operators_index = 0
+            display.show(0)
+        else:
+            display.show(operate())
+            sleep(5000)
+            second_number = False
+            first = 0
+            second = 0
+            numbers_index = 0
+            display.show(0)
+            break
+    
+    if button_a.is_pressed() and button_b.is_pressed():
+        while button_a.is_pressed() or button_b.is_pressed():
+            pass
+        display.show(Image(operators[operators_index % len(operators)]))
+        operators_index += 1
+
+    elif button_a.was_pressed():
+        sleep(200)
+        if not button_b.was_pressed():
+            numbers_index -= 1
+            display.show(numbers[numbers_index % len(numbers)])
+            if second_number:
+                second = numbers[numbers_index % len(numbers)]
+            else:
+                first = numbers[numbers_index % len(numbers)]
+    elif button_b.was_pressed():
+        sleep(200)
+        if not button_a.was_pressed():
+            numbers_index += 1
+            display.show(numbers[numbers_index % len(numbers)])
+            if second_number:
+                second = numbers[numbers_index % len(numbers)]
+            else:
+                first = numbers[numbers_index % len(numbers)]
