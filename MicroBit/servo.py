fdbd7cf737bb3af29dab9f562f324ce0d72a29cd
Torben0612 <74421064+Torben0612@users.noreply.github.com>
2023-05-04 18:16:39 -0400

    added survo.py

---
+++ MicroBit/servo.py
@@ -0,0 +1,14 @@
+from microbit import * 
+# Servo control: 
+# 50 = ~1 millisecond pulse all right 
+# 75 = ~1.5 millisecond pulse center 
+# 100 = ~2.0 millisecond pulse all left 
+pin0.set_analog_period(20)
+
+while True: 
+	pin0.write_analog(75)
+	sleep(1000)
+	pin0.write_analog(50)
+	sleep(1000)
+	pin0.write_analog(100)
+	sleep(1000)
